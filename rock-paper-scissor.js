8cabc8c155172063e65d79983a5e3e3fe822f7f3
pikaPush <renzgabuay@gmail.com>
2025-07-24 18:30:12 +0800

    Add logic to randomly select Rock, Paper, Scissor based on randomChoice

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,3 +1,11 @@
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3) + 1;
+
+  if (randomChoice === 1) {
+    return "Rock";
+  } else if (randomChoice === 2) {
+    return "Paper";
+  } else {
+    return "Scissor";
+  }
 }
