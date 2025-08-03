ba7f0be48925e706903ff03349f3b62185689606
pikaPush <renzgabuay@gmail.com>
2025-08-03 16:25:43 +0800

    Add DOM selectors for Rock, Paper, and Scissor buttons

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,6 +1,10 @@
 const result = document.getElementById("result");
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorButton = document.getElementById("scissorButton");
 
 function playRound(humanSelection, computerSelection) {
   const choices = ["Rock", "Paper", "Scissor"];
   computerSelection = choices[Math.floor(Math.random() * choices.length)];
+  console.log(computerSelection);
 }
