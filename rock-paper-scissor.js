8fcb51d183c23981c7f04405d30a93d118e660dd
pikaPush <renzgabuay@gmail.com>
2025-08-03 16:22:24 +0800

    Add logic to randomly select computer's choice from array

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -2,4 +2,5 @@ const result = document.getElementById("result");
 
 function playRound(humanSelection, computerSelection) {
   const choices = ["Rock", "Paper", "Scissor"];
+  computerSelection = choices[Math.floor(Math.random() * choices.length)];
 }
