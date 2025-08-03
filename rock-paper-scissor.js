61610815619e8be94a2c66039e6def8272169738
pikaPush <renzgabuay@gmail.com>
2025-08-03 18:18:12 +0800

    Initialize score and round tracking variables

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,2 +1,7 @@
 const buttons = document.querySelectorAll("button");
 const result = document.getElementById("result");
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRound = 1;
+let maxRound = 5;
