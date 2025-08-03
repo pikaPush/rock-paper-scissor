44e06281d0b2c731f59feeb940659d98cfa8feff
pikaPush <renzgabuay@gmail.com>
2025-08-03 18:20:10 +0800

    Add getComputerChoice() to randomly select computer's move
    Defined a function that selects a random choice from an array containing
    Rock, Paper, and Scissor. This will be used to generate the computer's
    selection each round, ensuring fair and unpredictable gameplay.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -5,3 +5,8 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRound = 1;
 let maxRound = 5;
+
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissor"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
